b5da2e2e20447bf70891643152d206a6d9e8dd65
Sabrina Bou <sabrinaanhari21@gmail.com>
2024-01-11 15:53:33 +0100

    inicio tarea2

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Evaluacion {
     Scanner sn= new Scanner(System.in);
 
-    private String nombreAsignatura;
+    private final String nombreAsignatura;
     private double[] listaNotas;
 
 
@@ -32,27 +32,29 @@ public class Evaluacion {
                 //volver a introducir las notas
                 listaNotas= new double[totalAlumnos];
 
-                //los arrays siempre empiezan por la posicion 0
+                //los arrays siempre empiezan por la posicion 0, y en este caso va aumentando
                 for (int i = 0; i < totalAlumnos; i++) {
                     //ir leyendo las notas y comprobando que se han introducido bien
                     System.out.print("Introduce la nota del alumno " + (i + 1) + ": ");
                     double nota = sn.nextDouble();
 
-                    if (i<0 || i>10){
+                    //si se introduce un numero menor oque 0 o mayor que 10
+                    if (nota<0 || nota>10){
                         System.out.println("Error: La nota debe estar en el rango de 0 a 10.");
                         /*hago que la i disminuya si se cumple la condicon, por ejemplo si en el
-                         * alumno 0 se introcuce un 11, en vez de que el for vaya a por el alumno
-                         * 1, baje y vuelva a introducir la nota del alumno 0*/
+                         * alumno 0 se introcuce un 11, en vez de que el for vaya a por la celda
+                         * 1, baje y vuelva a introducir la celda 0*/
                         i--;
                     }else {
+                        //si se ha introducido bien la nota, que se guarde en el array
                         listaNotas[i]= nota;
                     }
 
                 }
             }
-            //si no esta creado
+            //si no esta creado el array de notas
         }else{
-            //si el array no esta creado
+
             listaNotas= new double[totalAlumnos];
 
             for (int i = 0; i < totalAlumnos; i++) {
@@ -63,10 +65,11 @@ public class Evaluacion {
                 if (i<0 || i>10){
                     System.out.println("Error: La nota debe estar en el rango de 0 a 10.");
                     /*hago que la i disminuya si se cumple la condicon, por ejemplo si en el
-                     * alumno 0 se introcuce un 11, en vez de que el for vaya a por el alumno
-                     * 1, baje y vuelva a introducir la nota del alumno 0*/
+                     * alumno 0 se introcuce un 11, en vez de que el for vaya a por la celda
+                     * 1, baje y vuelva a introducir la celda 0*/
                     i--;
                 }else {
+                    //si se ha introducido bien la nota, que se guarde en el array
                     listaNotas[i]= nota;
                 }
 
@@ -77,7 +80,7 @@ public class Evaluacion {
         //mostrar las notas
         System.out.println("Notas introducidas: ");
         for (int i=0; i< listaNotas.length; i++){
-            System.out.print("Alumno " + (i+1)+ ": " + listaNotas[i]);
+            System.out.print("Alumno " + (i+1)+ ": " + listaNotas[i] + "\n ");
         }
     }
 
@@ -88,7 +91,8 @@ public class Evaluacion {
         if (listaNotas != null) {
             //si hay notas
 
-            //recorremos el array de notas y agregamos cada nota con su respectivo número de alumno
+            /*recorremos el array de notas y agregamos cada nota con su respectivo número de alumno, ejemplo,
+             cuando muestre la celda 0, imprimira, "Alumno 1: nota"*/
             for (int i = 0; i < listaNotas.length; i++) {
                 frase+= "Alumno " + (i + 1) + ": " + listaNotas[i];
             }
@@ -102,4 +106,269 @@ public class Evaluacion {
     }
 
 
+    public double media(){
+        double suma=0;
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas, empezamos a recorrer el array y sumando las notas que vayan apareciendo
+            for (int i=0; i< listaNotas.length; i++){
+                suma+= listaNotas[i];
+            }
+            //devuelve la suma de las notas entre la longitud del array
+            return suma / listaNotas.length;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    public double minimo(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            /*igualamos la variable a la primera nota del array, y a partir de esta
+            * vamos viendo si las celdas que vienen su contenido es menor que la primera celda */
+            double notaMenor= listaNotas[0];
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es menor que la celda 0, cambiar a celda actual
+                * si no es menor, sigue con el bucle*/
+                if(listaNotas[i]<notaMenor){
+                    notaMenor=listaNotas[i];
+                }
+            }
+            //devuelve la nota mas baja de entre la longitud del array
+            return notaMenor;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    public double maximo(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            /*igualamos la variable a la primera nota del array, y a partir de esta
+             * vamos viendo si las celdas que vienen su contenido es mayor que el anterior*/
+            double notaMayor= listaNotas[0];
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es mayor que la celda 0, cambiar a celda actual
+                 * si no es mayor, sigue con el bucle hasta que acabe la longitud del array*/
+                if(listaNotas[i]>notaMayor){
+                    notaMayor=listaNotas[i];
+                }
+            }
+            //devuelve la nota maxima de entre la longitud del array
+            return notaMayor;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    public int totalSuspensos(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            int suspensos= 0;
+            //el for recorre el array desde 0 hasta su ultima celda
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es menor que 5, aumentar contador de susoensos*/
+                if(listaNotas[i]<5){
+                    suspensos++;
+                }
+            }
+            //devuelve los suspensos que hay entre la longitud del array
+            return suspensos;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    //metodo para calcular el total de aprobados
+    public int totalAprobados(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            //el for recorre el array
+            int aprobadas= 0;
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es mayor igual que 5, aumentar contador de aprobados*/
+                if(listaNotas[i]>=5){
+                    aprobadas++;
+                }
+            }
+            //devuelve los aprobados que hay entre la longitud del array
+            return aprobadas;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    //metodo para cambiar nota, que no me devuelve nada
+    public void cambiarNota(double nota, int numAlumno){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+            //comprobar que el indice del alumno sea correcto
+            if(numAlumno>=0 && numAlumno< listaNotas.length){ //si el numero es mayor igual que 0 y esta dentro del rando del array
+                //comprobar que la nota introducida sea mayor igual que 0
+                if (nota>=0){
+                    listaNotas[numAlumno]= nota; //en el array, se introduce el indice del alumno, y se cambia la nota
+                    System.out.println("Alumno " + (numAlumno+1) + "se a cambiado a: " + nota);
+                }else{
+                    System.out.println("Nota introducida no válida.");
+                }
+
+            }else {
+                //si no coincide el indice introducido con las celdas del array listanotas
+                System.out.println("Indice del alumno incorrecto.");
+            }
+
+        }
+    }
+
+
+    //metodo para ver el alumno con mejor nota
+    public double mejorAlumno(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            /*igualamos la variable a la primera nota del array, y a partir de esta
+             * vamos viendo si las celdas que vienen su contenido es menor que el anterior*/
+            double notaMaxima= listaNotas[0];
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es mayor que la celda 0, cambiar a celda actual
+                 * si no es menor, sigue con el bucle*/
+                if(listaNotas[i]>notaMaxima){
+                    notaMaxima=listaNotas[i];
+                }
+            }
+            //devuelve la nota maxima de entre la longitud del array
+            return notaMaxima;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+    //metodo para ver el alumno con la nota mas baja
+    public double peorAlumno(){
+        //comprobar que el array no este vacio
+        if (listaNotas != null) {
+            //si hay notas
+
+            /*igualamos la variable a la primera nota del array, y a partir de esta
+             * vamos viendo si las celdas que vienen su contenido es menor que el anterior*/
+            double notaMinima= listaNotas[0];
+            for (int i=0; i< listaNotas.length; i++){
+                /*si el contenido de la celda actual es menor que la celda 0, cambiar a celda actual
+                 * si no es menor, sigue con el bucle*/
+                if(listaNotas[i]<notaMinima){
+                    notaMinima=listaNotas[i];
+                }
+            }
+            //devuelve la nota maxima de entre la longitud del array
+            return notaMinima;
+
+        } else {
+            //si no hay notas
+            return -1;
+        }
+    }
+
+
+    //metodo para introducir el indice del alumno y que me muestre la nota correspondiente
+    public double notaAlumno(int indiceAlumno){
+        if(indiceAlumno>0 && indiceAlumno<= listaNotas.length){
+            //si el numero es mayor que 0 y menor igual que el tamaño del array, que me devuelva la nota del alumno introducido
+            return listaNotas[indiceAlumno];
+        }else{
+            //si el indice esta fuera de rango
+            return -1;
+        }
+    }
+
+
+    //metodo para que me de un array donde esta el indice de los que alumnos estan aprobados
+    public int[] dameAprobados() {
+        //verificamos si hay notas en la lista
+        if (listaNotas != null && totalAprobados() > 0) {
+            //creamos un array y llamamos al  metodo totalAprobados
+            int[] arrayAprobados = new int[totalAprobados()];
+            int indiceArray = 0;
+
+            //bucle for para llenar el array con los índices de los alumnos aprobados
+            for (int i = 0; i < listaNotas.length; i++) {
+                //si la celda actual contiene nota mayor igual que 5, guardarlo en el array
+                if (listaNotas[i] >= 5.0) {
+                    arrayAprobados[indiceArray++] = i;
+                }
+            }
+
+            return arrayAprobados;
+        } else {
+            //si no hay aprobados o no hay notas, devolver null
+            return null;
+        }
+    }
+
+
+    //metodo para que me de un array donde esta el indice de los que alumnos estan suspendidos
+    public int[] dameSuspensos() {
+        //verificamos si hay notas en la lista
+        if (listaNotas != null && totalSuspensos() > 0) {
+            //creamos un array y llamamos al  metodo totalAprobados
+            int[] arraySuspensos = new int[totalSuspensos() ];
+            int indiceArray = 0;
+
+            // Utilizar un bucle for para llenar el array con los índices de los alumnos suspendidos
+            for (int i = 0; i < listaNotas.length; i++) {
+                //si la celda actual contiene nota menor que 5, guardarlo en el array suspensos
+                if (listaNotas[i] > 5.0) {
+                    arraySuspensos[indiceArray++] = i;
+                }
+            }
+
+            return arraySuspensos;
+        } else {
+            //si no hay aprobados o no hay notas, devolver null
+            return null;
+        }
+    }
+
+    //metodo para índice del primer alumno que cumpla que la nota es menor que la dada.
+    public int primerMenor(double nota){
+        //verificamos si hay notas en la lista
+        if (listaNotas != null) {
+            //utilizamos un bucle for para encontrar el índice del primer alumno con nota menor
+            for (int i = 0; i < listaNotas.length; i++) {
+                //comprobamos si la nota del alumno en la posición actual es menor que la nota dada
+                if (listaNotas[i] < nota) {
+                    //devolvemos el índice del alumno
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,30 +2,111 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args){
+        Evaluacion evaluacion= new Evaluacion("Programacion");
         int opcion;
-        int numero;
 
         do{
-            /*llamo a la función menu me muestra el menu
+            /*llamo a la función menu, me muestra el menu
               y con el switch me muestra cada opcion con su funcion*/
             opcion= mostrarMenu();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
+                    //introducir las notas
+                    System.out.print("Introduce el total de alumnos: ");
+                    int totalAlumnos = new Scanner(System.in).nextInt();
+                    evaluacion.leerNotas(totalAlumnos);
+                    break;
 
                 case 2:
+                    //mostrar notas
+                    System.out.println(evaluacion.toString());
+                    break;
 
                 case 3:
+                    //mostrar la media de las notas introducidas
+                    System.out.println("Media de las notas: " + evaluacion.media());
+                    break;
 
                 case 4:
+                    //mostrar la nota mas baja
+                    System.out.println("Nota mínima: " + evaluacion.minimo());
+                    break;
 
                 case 5:
+                    //mostrar la nota mas alta
+                    System.out.println("Nota máxima: " + evaluacion.maximo());
+                    break;
+
+                case 6:
+                    //mostrar el total de suspensos
+                    System.out.println("Total de suspensos: " + evaluacion.totalSuspensos());
+                    break;
+
+                case 7:
+                    //mostrar el total de aprobados
+                    System.out.println("Total aprobados: " + evaluacion.totalAprobados());
+                    break;
+
+                case 8:
+                    //cambiar nota
+                    System.out.print("Introduce el número del alumno: ");
+                    int numAlumno = new Scanner(System.in).nextInt();
+                    System.out.print("Introduce la nueva nota: ");
+                    double nuevaNota = new Scanner(System.in).nextDouble();
+                    evaluacion.cambiarNota(nuevaNota, numAlumno - 1);
+                    break;
+
+                case 9:
+                    //mostrar la nota del alumno con mas nota
+                    System.out.println("Mejor alumno: " + evaluacion.mejorAlumno());
+                    break;
+
+                case 10:
+                    //mostrar la nota del alumno con menos nota
+                    System.out.println("Peor alumno: " + evaluacion.peorAlumno());
+                    break;
+
+                case 11:
+                    //ver la nota de un alumno específico
+                    System.out.print("Introduce el número del alumno: ");
+                    int indiceAlumno = new Scanner(System.in).nextInt();
+                    System.out.println("Nota del alumno " + indiceAlumno + ": " + evaluacion.notaAlumno(indiceAlumno - 1));
+                    break;
+
+                case 12:
+                    //obtener el array de índices de aprobados
+                    int[] aprobados = evaluacion.dameAprobados();
+                    if (aprobados != null) {
+                        System.out.print("Índices de alumnos aprobados: ");
+                        for (int i : aprobados) {
+                            System.out.print(i + ", ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("No hay aprobados.");
+                    }
+                    break;
+
+                case 13:
+                    //obtener el array de índices de suspensos
+                    int[] suspensos = evaluacion.dameSuspensos();
+                    if (suspensos != null) {
+                        System.out.print("Índices de alumnos suspensos: ");
+                        for (int i : suspensos) {
+                            System.out.print(i + " ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("No hay suspensos.");
+                    }
+                    break;
 
                 default:
+                    break;
             }
 
-            /*si el numero introducido es 9
-            no volver a mostrar el menu, es decir sale del programa*/
-        }while(opcion != 9);
+
+        }while(opcion <=13);
 
     }
 
@@ -42,20 +123,27 @@ public class Principal {
         si no se introduce el numero adecuado*/
         do{
             System.out.println("Menú: ");
-            System.out.println("1. Tabla de multiplicar");
-            System.out.println("2. Saluda");
-            System.out.println("3. Números primos");
-            System.out.println("4. Sumar n enteros");
-            System.out.println("5. Multiplicar n enteros");
+            System.out.println("1. Introducir notas");
+            System.out.println("2. Mostrar notas");
+            System.out.println("3. Mostrar media");
+            System.out.println("4. Mostrar minimo");
+            System.out.println("5. Mostrar maximo");
+            System.out.println("6. Total suspensos");
+            System.out.println("7. Total aaprobados");
+            System.out.println("8. Cambiar nota");
+            System.out.println("9. Mejor alumno");
+            System.out.println("10. Peor alumno");
+            System.out.println("11. Ver nota alumno");
+            System.out.println("12. Dame total aprobados");
+            System.out.println("13. Dame total suspensos");
 
 
-            /*pedimos al usuario el numero
-             si el numero introducido es menor que 1 o mayor que 5
-             volver a mostrar el menu*/
+
+            /*pedimos al usuario el numero*/
             System.out.print("Introduce una opción del menú: ");
             opcion=sc.nextInt();
 
-        }while (opcion<1 || opcion>5);
+        }while (opcion>13);
 
         return opcion;
     }
