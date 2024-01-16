2562e9d3b501e34740979688c1dfa4c7a3797086
Sabrina Bou <sabrinaanhari21@gmail.com>
2024-01-16 05:27:39 +0100

    commmit nif

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,3 +1,12 @@
 public class Principal3 {
+    public static void main(String[] args){
 
+//        Tarea3String comprobar= new Tarea3String();
+//        comprobar.esCorrectoNIF("78299352L");
+//        if (comprobar.esCorrectoNIF("78299352L")){
+//            System.out.println("DNI introducido correcto");
+//        }else {
+//            System.out.println("DNI incorrecto, vurlve a intentarlo");
+//        }
+    }
 }
--- src/Tarea3String.java
+++ src/Tarea3String.java
@@ -1,5 +1,50 @@
 public class Tarea3String {
-    public static void main(String[] args){
 
+    /*metodo para comprobar que el nif introducido sea correcto.
+    * información obtenida de:
+    * https://es.stackoverflow.com/questions/325876/comprobar-si-un-car%C3%A1cter-de-un-string-es-un-n%C3%BAmero*/
+    public boolean esCorrectoNIF(String NIF){
+
+        //commprobamos q no este nulo o que exceda 9 caracteres
+        if (NIF== null || NIF.length()!=9){
+            return false;
+        }else {
+            String numDni = "";
+            /*vamos a recorrer los 8 digitos,
+            comprobando en todo momento que se trata de digitos y no letras*/
+            //desde la posicion 0, hasta la posicion 7
+            for (int i=0; i<8; i++){
+                //si el caracter actual, i, es diferente de digito, devolver falso
+                if(!Character.isDigit(NIF.charAt(i))){
+                    return false;
+                }else{  //si el caracter es un digito, almacenar, para utilizar despues para averiguar la laetra
+                    numDni+= NIF.charAt(i);
+                }
+
+            }
+
+            //convertir del String NIF, el caracter 8(java empieza a contar desde 0), convertirlo en un caracter
+            char letra= NIF.charAt(8);
+            //comprobar que el char sea una letra
+            if (!Character.isLetter(letra)){
+                return false;
+            }else{
+                //crear array con las letras correctas que puede tener un DNI
+                char[] letrasDni= {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+                /*como numdni es un string, tengo que pasarlo a int primero, y despues calcular el modulo*/
+                int numero= Integer.parseInt(numDni);
+                //con el nuevo int, calculo el modulo para averiguar la letra correcta
+                int modulo= numero%23;
+
+                //en base al modulo obtenido, obtener letra del array, ejemplo, si el modulo es 3, me devolvera 'A'
+                char letraCalculada= letrasDni[modulo];
+
+                //si la letra proporcioanada en el dni, coincide con la del array, devolver true
+                return letra== letraCalculada;
+            }
+
+        }
     }
+
+
 }
